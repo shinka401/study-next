d1c59a581adb96c2cedfbf5e26b9fbb5c8e8c819
shinka401 <shinka401unauthorized@gmail.com>
2023-02-18 19:52:54 +0900

    ♻️ コンポーネント化

---
+++ component/HeadLine.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+type HeadLineProps = {
+  title: string;
+};
+
+export function HeadLine(props: HeadLineProps) {
+  return (
+    <div className={styles.description}>
+      <p>
+        Get started by editing&nbsp;
+        <code className={styles.code}>pages/{props.title}.tsx</code>
+      </p>
+      <div>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          By{" "}
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.vercelLogo}
+            width={100}
+            height={24}
+            priority
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
